7647f9b6f4636ada8cab2e07f0a207f13b3b1c26
icodezjb <icodezjb@163.com>
2019-04-18 11:18:37 +0800

    Add filterLogs

--- contract/htlc_test.go
+++ contract/htlc_test.go
@@ -12,24 +12,38 @@ import (
 
 	"github.com/icodezjb/atomicswap/contract/helper"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 const hourSeconds = 3600
+const oneFinney = 10^15
+const port  = "7545"
+
 
 type Ganache struct  {
 	cmd *exec.Cmd
 	running bool
 }
 
+type LogHTLC struct {
+	ContractId []byte
+	From		common.Address
+	To			common.Address
+	Value   	*big.Int
+	HashLock 	[]byte
+	TimeLock	*big.Int
+}
+
 var ganache = Ganache {
 	cmd: exec.Command(
 		"/usr/local/bin/ganache-cli",
 		"--account", "0xa5a1aca01671e2660f1ee47abfd7065d5d38f99fa4a53495f02df939cd5b86f6,111111111111111111111",
-		"-p", "7545"),
+		"-p", port),
 	running:true,
 }
 
@@ -86,6 +100,24 @@ func makeAuth(t *testing.T, private string, client *ethclient.Client, value int6
 	return auth
 }
 
+func filterLogs(t *testing.T, contractAddress common.Address) []types.Log {
+	query := ethereum.FilterQuery{
+		Addresses:[]common.Address{contractAddress},
+	}
+
+	client, err := ethclient.Dial("ws://127.0.0.1:" + port)
+	if err != nil {
+		t.Fatal("Fatal new websocket client ", err)
+	}
+
+	logs, err := client.FilterLogs(context.Background(), query)
+	if	err	!=	nil	{
+		t.Fatal("Fatal new FilterLogs ", err)
+	}
+
+	return logs
+}
+
 func TestNewContract(t *testing.T) {
 	setup()
 	defer cleanup()
@@ -103,17 +135,19 @@ func TestNewContract(t *testing.T) {
 	senderAuth := makeAuth(t, senderKey, client, 0)
 
 	//Deploy contract
-	_, _, instance, err := DeployHtlc(senderAuth, client)
+	contractAddress, _, instance, err := DeployHtlc(senderAuth, client)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	senderAuth = makeAuth(t, senderKey, client, 10^18)
+	senderAuth = makeAuth(t, senderKey, client, oneFinney)
 
-	tx, err := instance.NewContract(senderAuth, receiver, hashPair.Hash,big.NewInt(timeLock1Hour))
+	_, err = instance.NewContract(senderAuth, receiver, hashPair.Hash,big.NewInt(timeLock1Hour))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	t.Log(tx)
+	logs := filterLogs(t, contractAddress)
+
+	t.Log(logs)
 }
