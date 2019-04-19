91ba891333702eed9432c01f54d8cfffd3eff51f
icodezjb <icodezjb@163.com>
2019-04-19 13:31:16 +0800

    Add TestNewContractWithPastTimelocks

--- contract/htlc_test.go
+++ contract/htlc_test.go
@@ -234,3 +234,33 @@ func TestNewContractWithNoETH(t *testing.T) {
 		t.Fatal("expected failure due to 0 value transferred")
 	}
 }
+
+//newContract() should fail with timelocks in the past
+func TestNewContractWithPastTimelocks(t *testing.T)  {
+	setup()
+	defer cleanup()
+
+	var timeLockPast = time.Now().Unix() - 1
+	var senderKey   = "a5a1aca01671e2660f1ee47abfd7065d5d38f99fa4a53495f02df939cd5b86f6"
+	var receiver = common.HexToAddress("0x5eb1231fd20ac35dff0d4295959cbd11c9cdae40")
+	var hashPair = htlc.NewSecretHashPair()
+
+	client, err := ethclient.Dial("http://127.0.0.1:7545")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	senderAuth := makeAuth(t, senderKey, client, 0)
+
+	_, _, instance, err := DeployHtlc(senderAuth, client)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	senderAuth = makeAuth(t, senderKey, client, oneFinney)
+
+	_, err = instance.NewContract(senderAuth, receiver, hashPair.Hash, big.NewInt(timeLockPast))
+	if !(err != nil && strings.HasPrefix(err.Error(),REQUIRE_FAILED_MSG)) {
+		t.Fatal("expected failure due past timelock")
+	}
+}
