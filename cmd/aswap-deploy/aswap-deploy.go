7acd02d725d424aa86b4062c5bce80ad0c3e3999
icodezjb <icodezjb@163.com>
2019-11-27 15:05:52 +0800

    Update aswap-deploy usage

--- cmd/aswap-deploy/aswap-deploy.go
+++ cmd/aswap-deploy/aswap-deploy.go
@@ -31,12 +31,49 @@ func VersionFunc() string {
 }
 
 func init() {
-	rootCmd.Flags().StringVar(
+	rootCmd.Flags().StringVarP(
 		&dHandle.ConfigPath,
 		"config",
+		"c",
 		"./config.json",
 		"config file path",
 	)
+
+	rootCmd.SetUsageTemplate(`Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+
+config.json example
+{
+  "chainId": 1,
+  "chainName": "eth",
+  "url": "http://127.0.0.1:7545",
+  "from": "0xffd79941b7085805f48ded97298694c6bb950e2c",
+  "keystoreDir": "./",
+  "password": "111111"
+}
+`)
+
 }
 
 func main() {
